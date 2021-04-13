c5cccc3bb693798e7b8db95a0045187c2111bddd
kahyunKo <78471342+kahyunKo@users.noreply.github.com>
2021-04-13 21:22:23 +0900

    Add files via upload

---
+++ ch01_02-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double c_temp = 20;
+	double f_temp;
+
+	f_temp = (9.0 / 5.0 * c_temp) + 32;
+
+	cout << "¼·¾¾¿Âµµ " << c_temp << "µµ´Â È­¾¾¿Âµµ " << f_temp << "µµÀÔ´Ï´Ù." << endl;
+
+	return 0;
+}
